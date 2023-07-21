0a80d588a743f40a66c1100c8bfd0741a09d86b6
lalaggv2 <luangu2020@gmail.com>
2023-07-20 22:29:19 -0500

    cambio mensaje a copiado

--- script.js
+++ script.js
@@ -44,5 +44,5 @@ function desencriptar(stringDesencriptado) {
 function copiar() {
     mensaje.select();
     navigator.clipboard.writeText(mensaje.value);
-    alert("Copied")
+    alert("Copiado")
 }
